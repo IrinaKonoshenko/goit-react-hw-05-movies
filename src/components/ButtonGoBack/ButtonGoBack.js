42d11109383135c90b04cdedf71e0f5a86f10d3d
Irina Konoshenko <ayren.syssoyeva@gmail.com>
2023-01-02 19:31:24 +0600

    fix 5

--- src/components/ButtonGoBack/ButtonGoBack.js
+++ src/components/ButtonGoBack/ButtonGoBack.js
@@ -2,7 +2,7 @@ import { Link, useLocation } from 'react-router-dom';
 
 export function ButtonGoBack() {
   const location = useLocation();
-  const backLinkHref = location.state?.from ?? '/products';
+  const backLinkHref = location.state?.from ?? '/';
 
   return <Link to={backLinkHref}>Go back </Link>;
 }
